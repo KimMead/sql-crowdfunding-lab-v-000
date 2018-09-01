06df66ad4b38d3cd825e7ec0984dfaaf801d6ec5
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-09-01 17:27:12 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT Projects.title, Pledges.amount
 FROM Projects
 INNER JOIN Pledges
-ON Projects.id = Pledges.project_id"
+ON Projects.id = Pledges.project_id
+ORDER BY name"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
